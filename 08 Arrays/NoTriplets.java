3568f08686a07ec2485d9a35bfd9fc1a711aca4a
isinghabhishek <singhabhishek3006.abhi@gmail.com>
2026-04-15 00:46:58 +0530

    update: notriplets

--- 08 Arrays/NoTriplets.java
+++ 08 Arrays/NoTriplets.java
@@ -1,19 +1,17 @@
 public class NoTriplets {
 
     public static int noTriplets(int nums[]){
-
+        int count = 0;
         for(int i=0; i<nums.length; i++){
-            for(int j=0; j<nums.length; j++){
-                for(int k=0; k<nums.length; k++){
-                    if(i != j || j != k || j != k || nums[i]+nums[j]+nums[k] == 0){
-                        
+            for(int j=i+1; j<nums.length; j++){
+                for(int k=j+1; k<nums.length; k++){
+                    if(nums[i]+nums[j]+nums[k] == 0){
+                        count++;
                     }
                 }
-                
             }
-            
         }
-        
+        return count;
     }
     public static void main(String[] args) {
         int nums[] = {-1, 0, 1, 2, -1, -4};
